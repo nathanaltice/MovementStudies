e18d0cf81efd236b078ed1bf053016524f25e0da
Nathan Altice <naltice@ucsc.edu>
2023-10-18 14:40:47 -0700

    code cleanup

--- scenes/Velocity.js
+++ scenes/Velocity.js
@@ -1,39 +1,40 @@
 class Velocity extends Phaser.Scene {
     constructor() {
-        super('velocityScene');
+        super('velocityScene')
     }
 
     create() {
         // variables and settings
-        this.MAX_VELOCITY = 300;    // pixels/second
-        this.physics.world.gravity.y = 1000;
+        this.MAX_VELOCITY = 300    // pixels/second
+        this.physics.world.gravity.y = 1000
 
         // set bg color
-        this.cameras.main.setBackgroundColor('#CCC');
+        this.cameras.main.setBackgroundColor('#CCC')
 
         // print Scene name
-        this.add.text(game.config.width/2, 30, 'Scene 1: Velocity Moves (with Grumpy Fly Buddy)', { font: '14px Futura', fill: '#FFFFFF' }).setOrigin(0.5);
-        this.add.text(game.config.width/2, 50, '(Number Keys 1–7 Start Their Respective Scene Numbers)', { font: '14px Futura', fill: '#FFFFAA' }).setOrigin(0.5);
+        this.add.text(game.config.width/2, 30, 'Scene 1: Velocity Moves (with Grumpy Fly Buddy)', { font: '14px Futura', fill: '#FFFFFF' }).setOrigin(0.5)
+        this.add.text(game.config.width/2, 50, '(Number Keys 1–7 Start Their Respective Scene Numbers)', { font: '14px Futura', fill: '#FFFFAA' }).setOrigin(0.5)
 
         // make ground tiles
-        this.ground = this.add.group();
+        this.ground = this.add.group()
         for(let i = 0; i < game.config.width; i += tileSize) {
-            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'platformer_atlas', 'block').setScale(SCALE).setOrigin(0);
-            groundTile.body.immovable = true;
-            groundTile.body.allowGravity = false;
-            this.ground.add(groundTile);
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'platformer_atlas', 'block').setScale(SCALE).setOrigin(0)
+            groundTile.body.immovable = true
+            groundTile.body.allowGravity = false
+            this.ground.add(groundTile)
         }
 
         // add some physics clouds
-        this.cloud01 = this.physics.add.sprite(600, 100, 'platformer_atlas', 'cloud_1');
-        this.cloud01.body.setAllowGravity(false).setVelocityX(25);
-        this.cloud02 = this.physics.add.sprite(200, 200, 'platformer_atlas', 'cloud_2');
-        this.cloud02.body.setAllowGravity(false).setVelocityX(45);
+        this.cloud01 = this.physics.add.sprite(600, 100, 'platformer_atlas', 'cloud_1')
+        this.cloud01.body.setAllowGravity(false).setVelocityX(25)
+        this.cloud02 = this.physics.add.sprite(200, 200, 'platformer_atlas', 'cloud_2')
+        this.cloud02.body.setAllowGravity(false).setVelocityX(45)
 
-        // set up my alien son 👽
+        // add the alien 👽
         // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/arcade-gameobject/#sprite-object
-        this.alien = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'platformer_atlas', 'front').setScale(SCALE);
-        this.alien.setCollideWorldBounds(true);
+        this.alien = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'platformer_atlas', 'front').setScale(SCALE)
+        this.alien.setCollideWorldBounds(true)
+       
         // create alien animations from texture atlas
         // see: https://photonstorm.github.io/phaser3-docs/Phaser.Types.Animations.html#toc1__anchor
         // key: string, frames: array, frameRate: int, repeat: int
@@ -50,7 +51,7 @@ class Velocity extends Phaser.Scene {
             }), 
             frameRate: 30,
             repeat: -1 
-        });
+        })
         this.anims.create({
             key: 'idle',
             defaultTextureKey: 'platformer_atlas',
@@ -58,7 +59,7 @@ class Velocity extends Phaser.Scene {
                 { frame: 'front' }
             ],
             repeat: -1
-        });
+        })
         // won't need this for now, but we're taking care of it for a future scene
         this.anims.create({
             key: 'jump',
@@ -66,10 +67,10 @@ class Velocity extends Phaser.Scene {
             frames: [
                 { frame: 'jump' }
             ],
-        });
+        })
 
-        // make a fly friend 🦟
-        this.fly = this.add.sprite(game.config.width/2 + 150, game.config.height/2, 'platformer_atlas', 'fly_normal').setScale(SCALE);
+        // add a fly friend 🦟
+        this.fly = this.add.sprite(game.config.width/2 + 150, game.config.height/2, 'platformer_atlas', 'fly_normal').setScale(SCALE)
         this.anims.create({
             key: 'flyflap',
             frames: [
@@ -80,80 +81,80 @@ class Velocity extends Phaser.Scene {
             repeat: -1
         });
         // go ahead and start the flapping animation since the fly is non-interactive
-        this.fly.anims.play('flyflap');
+        this.fly.anims.play('flyflap')
 
         // add arrow key graphics as UI
-        this.upKey = this.add.sprite(64, 32, 'arrowKey');
-		this.leftKey = this.add.sprite(32, 64, 'arrowKey');
-		this.downKey = this.add.sprite(64, 64, 'arrowKey');
-		this.rightKey = this.add.sprite(96, 64, 'arrowKey');
-		this.leftKey.rotation = Math.PI/2*3;
-		this.downKey.rotation = Math.PI;
-        this.rightKey.rotation = Math.PI/2;
-        this.upKey.tint = 0x333333;
-        this.downKey.tint = 0x333333;
+        this.upKey = this.add.sprite(64, 32, 'arrowKey')
+		this.leftKey = this.add.sprite(32, 64, 'arrowKey')
+		this.downKey = this.add.sprite(64, 64, 'arrowKey')
+		this.rightKey = this.add.sprite(96, 64, 'arrowKey')
+		this.leftKey.rotation = Math.PI/2*3
+		this.downKey.rotation = Math.PI
+        this.rightKey.rotation = Math.PI/2
+        this.upKey.tint = 0x333333
+        this.downKey.tint = 0x333333
 
         // set up Phaser-provided cursor key input
-        cursors = this.input.keyboard.createCursorKeys();
+        cursors = this.input.keyboard.createCursorKeys()
 
         // add physics collider
-        this.physics.add.collider(this.alien, this.ground);
+        this.physics.add.collider(this.alien, this.ground)
 
         // set up Scene switcher
         // note: this style of scene switching is for demo purposes only
         this.input.keyboard.on('keydown', (event) => {
-            //console.log(event);
+            //console.log(event)
             switch(event.key) {
                 case '1':
-                    this.scene.start('velocityScene');
-                    break;
+                    this.scene.start('velocityScene')
+                    break
                 case '2':
-                    this.scene.start('accelerationScene');
-                    break;
+                    this.scene.start('accelerationScene')
+                    break
                 case '3':
-                    this.scene.start('fixedJumpScene');
-                    break;
+                    this.scene.start('fixedJumpScene')
+                    break
                 case '4':
-                    this.scene.start('variableJumpScene');
-                    break;
+                    this.scene.start('variableJumpScene')
+                    break
                 case '5':
-                    this.scene.start('runnerScene');
-                    break;
+                    this.scene.start('runnerScene')
+                    break
                 case '6':
-                    this.scene.start('pogoScene');
-                    break;
+                    this.scene.start('pogoScene')
+                    break
                 case '7':
-                    this.scene.start('asteroidsScene');
-                    break;
+                    this.scene.start('asteroidsScene')
+                    break
                 default:
-                    break;
+                    break
             }
-        });
+        })
     }
 
     update() {
         // check keyboard input
         if(cursors.left.isDown) {
-            this.alien.setVelocityX(-this.MAX_VELOCITY);
-            this.alien.setFlip(true, false);
+            this.alien.setVelocityX(-this.MAX_VELOCITY)
+            this.alien.setFlip(true, false)
             // see: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Components.Animation.html#play__anchor
             // play(key [, ignoreIfPlaying] [, startFrame])
-            this.alien.anims.play('walk', true);
-            this.leftKey.tint = 0xFACADE;   // tint key
+            this.alien.anims.play('walk', true)
+            this.leftKey.tint = 0xFACADE   // tint key
         } else if(cursors.right.isDown) {
-            this.alien.setVelocityX(this.MAX_VELOCITY);
-            this.alien.resetFlip();
-            this.alien.anims.play('walk', true);
-            this.rightKey.tint = 0xFACADE;  // tint key
+            this.alien.setVelocityX(this.MAX_VELOCITY)
+            this.alien.resetFlip()
+            this.alien.anims.play('walk', true)
+            this.rightKey.tint = 0xFACADE  // tint key
         } else {
-            this.alien.body.velocity.x = 0;
-            this.alien.anims.play('idle');
-            this.leftKey.tint = 0xFFFFFF;   // un-tint keys
-            this.rightKey.tint = 0xFFFFFF;  
+            this.alien.body.velocity.x = 0
+            this.alien.anims.play('idle')
+            this.leftKey.tint = 0xFFFFFF   // un-tint keys
+            this.rightKey.tint = 0xFFFFFF  
         }
 
         // wrap physics object(s) .wrap(gameObject, padding)
-        this.physics.world.wrap(this.cloud01, this.cloud01.width/2);
-        this.physics.world.wrap(this.cloud02, this.cloud02.width/2);
+        this.physics.world.wrap(this.cloud01, this.cloud01.width/2)
+        this.physics.world.wrap(this.cloud02, this.cloud02.width/2)
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 // Nathan Altice
-// Updated: 7/1/20
+// Updated: 10/18/23
 // Phaser 3 Movement Studies
 // Concepts: Arcade physics, atlas and atlasXML loading, atlas animation (custom and generated frames), physics world wrapping, physics body properties (velocity, acceleration, drag, max acceleration), keyboard (isDown, JustPressed, DownDuration, UpDuration)
 // Some mechanics inspired by and adapted from Game Mechanic Explorer https://gamemechanicexplorer.com
 // The two example atlases are commercial assets and should not be used for your own projects - buy them from https://www.kenney.nl/assets :)
 
 // tame the javashrek
-'use strict';
+'use strict'
 
 // global variables
-let cursors;
-let currentScene = 0;
-const SCALE = 0.5;
-const tileSize = 35;
+let cursors
+const SCALE = 0.5
+const tileSize = 35
 
 // main game object
 let config = {
@@ -30,6 +29,6 @@ let config = {
         }
     },
     scene: [ Load, Velocity, Acceleration, FixedJump, VariableJump, Runner, Pogo, Asteroids ]
-};
+}
 
-let game = new Phaser.Game(config);
+let game = new Phaser.Game(config)
